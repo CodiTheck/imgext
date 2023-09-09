ef8dc75999bbbc3abcbded5dad50833479153693
Dr Mokira <arnoldmokira3d48@gamil.com>
2023-09-09 07:18:21 -0800

    setup.cfg configuration is done.

--- src/imgext/__main__.py
+++ src/imgext/__main__.py
@@ -16,6 +16,7 @@ FILE_TYPE_ALLOWED = (
 
 
 def main() -> int:
+    """ Main function. """
     file_path = ''
     if len(os.sys.argv) < 2:
         file_path = askopenfilename(
@@ -28,7 +29,7 @@ def main() -> int:
     img_files = extract_img(file_path, dir_path)
     print("\nimage files list:")
     for file_path in img_files:
-        print(f"  * saved at {file_path}", flush=True)
+        print(f"  \033[92m*\033[0m saved at {file_path}", flush=True)
 
     return 0
 
@@ -37,5 +38,5 @@ if __name__ == '__main__':
     try:
         os.sys.exit(main())
     except KeyboardInterrupt:
-        print("\033[91mCanceled by user.\033[0m")
-        os.sys.exit(125) 
+        os.sys.write("\033[91mCanceled by user.\033[0m")
+        os.sys.exit(125)
